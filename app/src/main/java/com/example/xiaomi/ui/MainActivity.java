1245aad6291742420825cf6a83aba44f70820dbe
raullek <42517310+raullek@users.noreply.github.com>
2019-01-17 17:25:32 +0400

    Initial commit

---
+++ app/src/main/java/com/example/xiaomi/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.xiaomi.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.xiaomi.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+    }
+}
---
+++ app/src/main/java/com/example/xiaomi/ui/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.example.xiaomi.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.xiaomi.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
